1752a4c7934405600b2c369616a38710d72ea443
Maxime CAVARROC <maxime.cavarroc@viacesi.fr>
2021-03-19 22:57:18 +0100

    constructeur set

--- controllers/Table.php
+++ controllers/Table.php
@@ -1,6 +1,9 @@
 <?php
 class Table {
     protected $_id;
+    public function __construct(array $data) {
+        $this->hydrate($data);
+    }
     public function hydrate(array $data) {
         foreach ($data as $key => $value) {
             $key = '_'.$key;
